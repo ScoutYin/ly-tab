7381710296908b57651080ca8743a30e6073f97a
ScoutYin <scoutyin.1900@gmail.com>
2022-04-25 17:47:34 +0800

    feat: global usage support

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import LyTabBar from './tab-bar.vue';
 import LyTabItem from './tab-item.vue';
 import LyTabs from './tabs.vue';
 
+const install = (Vue) => {
+	Vue.component(LyTabs.name, LyTabs);
+	Vue.component(LyTabItem.name, LyTabItem);
+	Vue.component(LyTabBar.name, LyTabBar);
+};
+
+export default { install };
+
 export { LyTabs, LyTabBar, LyTabItem };
